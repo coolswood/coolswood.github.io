cc6f8096126ef0fb5a1882fc1164ae0ad99a97c4
coolswood <coolswood@yandex.ru>
2017-03-24 21:15:13 +0300

    Проповеди доработка

--- primirenie/js/main.js
+++ primirenie/js/main.js
@@ -1,9 +1,11 @@
+// Карусели на главной
+
 if(document.body.clientWidth<600){
-   
+
    var gallery = document.querySelector('.gallery-wrapper');
    gallery.className += ' owl-carousel owl-theme';
 
-   
+
    $('.owl-carousel').owlCarousel({
    loop:true,
    margin:10,
@@ -13,5 +15,34 @@ if(document.body.clientWidth<600){
       }
    }
    });
-      
+
 };
+
+$('.owl-carousel-pastor').owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+// Карусели на главной
+
+
+// Проповеди
+
+
+//let open = document.querySelectorAll('.play-close');
+//for(i=0;i<open.length;i++) {
+//
+//   prop[i].style.height='150px';
+//
+//}
