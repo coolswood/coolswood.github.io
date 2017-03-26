ed083cde9304321c270084a2559f245d825ff148
coolswood <coolswood@yandex.ru>
2017-03-26 22:39:06 +0300

    Категории проповедей

---
+++ primirenie/Propoved_pleer/Book_of_Revelation/js/myplaylist.js
@@ -0,0 +1,93 @@
+var myPlaylist = [
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Урок о побеждающих',
+        artist:'Светлана Зайко',
+        rating:0,
+        buy:'mix/1.mp3',
+        duration:'31 декабря 2006',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Послание Лаодикийской церкви',
+        artist:'Сергей Хрипунов',
+        rating:0,
+        buy:'mix/1.mp3',
+        duration:'24 декабря 2006',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Послание Филадельфийской церкви',
+        artist:'Эдуард Зайко',
+        rating:0,
+        buy:'mix/1.mp3',
+        duration:'17 декабря 2006',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Послание Сардийской церкви',
+        artist:'Сергей Хрипунов',
+        rating:0,
+        buy:'mix/1.mp3',
+        duration:'10 декабря 2006',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Послание Фиатирской церкви',
+        artist:'Эдуард Зайко',
+        rating:0,
+        buy:'mix/1.mp3',
+        duration:'3 декабря 2006',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Послание Пергамской церкви',
+        artist:'Эдуард Зайко',
+        rating:0,
+        buy:'mix/1.mp3',
+        duration:'26 ноября 2006',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Послание Смирнской церкви',
+        artist:'Сергей Хрипунов',
+        rating:0,
+        buy:'mix/1.mp3',
+        duration:'19 ноября 2006',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Послание Ефесской церкви',
+        artist:'Эдуард Зайко',
+        rating:0,
+        buy:'mix/1.mp3',
+        duration:'12 ноября 2006',
+        cover:'mix/1.png'
+    },
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Обзор 1 главы Откровения',
+        artist:'Сергей Хрипунов',
+        rating:0,
+        buy:'mix/1.mp3',
+        duration:'5 ноября 2006',
+        cover:'mix/1.png'
+    },
+
+];
