e93e164c8b8e7ae2a830965cc831a76cad21ff55
coolswood <coolswood@yandex.ru>
2019-04-12 23:49:04 +0300

    translate

--- js/translate.js
+++ js/translate.js
@@ -1,3 +1,19 @@
+let description = `
+<span>My name is Daniel and I am a web developer with more than 3 years of experience. My technology stack mostly concerns front-end development, recently I also used back-end technologies on my personal projects.</span>
+<span>My main goal is to develop in JavaScript, it is a dynamic and rich language for which the future. At this stage, it allows you to write client, server, desktop cross-platform, mobile applications, it opens up huge opportunities for development.</span>
+<span>At the moment I specialize in front-end development of SPA applications. Main stack - React + Redux.</span>
+<span>I had experience with SSR. While I do not plan to change my main stack, React is the dominant js library and is developing rapidly. I would like to add more work with node to the stack.</span>
+<b>About soft skills:</b>
+<span>I feel free to communicate with colleagues and management. For the head I can present the project, justify the need for the use of architecture and technology. I also have no problems communicating with colleagues. In a formal working environment, I do not hesitate to ask if something is unclear to me from a more experienced colleague, I am happy to share my knowledge with a less experienced one. Do love and know how to explain (my coaching skills).</span>
+<span>Behind the doors of the office I lead an active social life, professionally - work as a mentor, mitaps, a small YouTube channel about technologies.</span>
+<span>In non-professional - martial arts, performances in children's theater, English club.</span>
+<b>About hard skills:</b>
+<span>As I wrote above, I work with the React + Redux stack. Also had experience with Flux.</span>
+<span>Actively exploring server-side JavaScript - node.js, working with mongodb database. In my portfolio you can see examples of projects where these technologies were used. Basically, it is the creation of API, reading/writing to the database, parsing information from third-party resources.</span>
+<span>There are also examples of desktop applications based on Electron created by me.</span>
+<span>I love and know how to optimize as a classic site – optimization of graphic images, drawing sprites, dynamic loading of content, the allocation of critical css and so on., and SPA applications – the correct configuration of the webpack configuration, import only used functions from the modules, the division of the configuration into bundles, etc.</span>
+`
+
 const en = [
     ["tr-hi", "Hello"],
     ["tr-my-name", "My name is Daniel, I am a web-developer"],
@@ -23,7 +39,8 @@ const en = [
     ["tr-teacher", "teacher in HTML Academy"],
     ["tr-npm", "NPM libraries"],
     ["tr-html_1", "HTML & CSS, level 1"],
-    ["tr-students", "students"]
+    ["tr-students", "students"],
+    ["tr-description", description]
 ];
 
 if(document.location.search === '?en') {
