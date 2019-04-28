1b98d890c5c6aa76e3a1eb7cfd90dd23de28f8ee
coolswood <coolswood@yandex.ru>
2019-04-28 22:51:24 +0300

    diary

---
+++ projects/diary/precache-manifest.f9e7192494f1a5cd738ddef3ee339546.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7377329312f1634a6c19dc02193927e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "790be3bb3792e3e059e9",
+    "url": "/static/css/main.6d6b5a8b.chunk.css"
+  },
+  {
+    "revision": "e084d1dd01ae681c1227",
+    "url": "/static/js/2.79d7762a.chunk.js"
+  },
+  {
+    "revision": "790be3bb3792e3e059e9",
+    "url": "/static/js/main.212f1a8d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
